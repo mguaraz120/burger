3f8eca0688757822103493348514bab3d2520e47
Mario Vizcaino <vizhar1952@gmail.com>
2019-12-24 16:11:28 -0500

    created config folder, db folder and files to begin workin on the app

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Awesome Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("ok Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Good Burger", true);
