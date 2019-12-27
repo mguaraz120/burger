74354aed2d4914ecc9de4004d0555dc27fa9ac31
Mario Vizcaino <vizhar1952@gmail.com>
2019-12-27 11:21:08 -0500

    added an image and css folder and added a create function in orm file

--- config/orm.js
+++ config/orm.js
@@ -34,5 +34,23 @@ var orm = {
     });
   },
 
-}
+
+create: function(table, cols, vals, cb) {
+  var queryString = "INSERT INTO " + table;
+
+  queryString += " (";
+  queryString += cols.toString();
+  queryString += ") ";
+  queryString += "VALUES (";
+  queryString += printQuestionMarks(vals.length);
+  queryString += ") ";
+
+  connection.query(queryString, vals, function(err, result) {
+    if (err) {
+      throw err;
+    }
+    cb(result);
+  });
+},
+};
 module.exports = orm;
